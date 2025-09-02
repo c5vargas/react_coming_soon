07fbe652a1433d895dd642a5ff81e89de92e9449
Carles Vargas <carlesvargas5@gmail.com>
2025-09-02 13:17:23 +0200

    fix(.env): added logo url env var

--- src/Icons.jsx
+++ src/Icons.jsx
@@ -66,6 +66,6 @@ export function ArrowRightIcon({className = 'w-6 h-6'}) {
 
 export function TwitterIcon(props) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m3 21l7.548-7.548M21 3l-7.548 7.548m0 0L8 3H3l7.548 10.452m2.904-2.904L21 21h-5l-5.452-7.548" color="currentColor"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="m3 21l7.548-7.548M21 3l-7.548 7.548m0 0L8 3H3l7.548 10.452m2.904-2.904L21 21h-5l-5.452-7.548" color="currentColor"/></svg>
   )
 }
